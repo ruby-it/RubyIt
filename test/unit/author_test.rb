bab8a13c57bd82b6f66b38511e2a38b6d9fa8dfa
Andrea Pavoni <andrea.pavoni@gmail.com>
2011-07-04 19:29:55 +0200

    rewrote tests for Author model

--- test/unit/author_test.rb
+++ test/unit/author_test.rb
@@ -1,26 +1,23 @@
 require 'test_helper'
 
 class AuthorTest < Test::Unit::TestCase
-  def setup
-    @author = Author.find(1)
+
+  # validations
+  def test_validates_presence_of_name
+    author = Factory.build :author, :name => nil
+    assert !author.valid?
+    assert author.errors.has_key?(:name)
+
+    author.name = 'Pippo'
+    assert author.valid?
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert_kind_of Author,  @author
+  def test_validates_presence_of_ip
+    author = Factory.build :author, :ip => nil
+    assert !author.valid?
+    assert author.errors.has_key?(:ip)
+
+    author.ip = '10.0.0.1'
+    assert author.valid?
   end
-  def test_find_or_create
-    david1=Author.find_by_name 'David'
-    david2=Author.find_or_create_by_name_and_ip 'David','0.0.0.0'
-    assert_equal david2, david1
-    none=Author.find_by_name 'non esiste'
-    assert_nil none
-    
-    none1=Author.find_or_create_by_name_and_ip 'non esiste','1.2.3.4'
-    none2=Author.find_by_name 'non esiste'
-    assert_equal none2, none1
-
-  end
-
-
 end
