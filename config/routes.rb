4e1cbf11beed177349275b38c708b9ca0053310c
Andrea Pavoni <andrea.pavoni@gmail.com>
2011-07-04 19:04:44 +0200

    ported routes from old app

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,22 @@
 RubyIt::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  root :to => 'home#index'
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  match 'pages/:page_title/revisions/new/:revision_number' => 'revisions#new', :constraints => {:revision_number => /\d+/}, :as => :rollback
+  match 'pages/:page_title/revisions/new' => 'revisions#new', :as => :new
+  match 'pages/:page_title/revisions/:revision_number' => 'revisions#show', :constraints => {:revision_number => /\d+/}, :as => :revision
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
+  #FIXME: something is useless, cleanup
+  # menu items, actually
+  get 'recent' => 'pages#recent'
+  get 'pages/index' => 'pages#index', :as => :pages
+  get 'home' => 'home#index'
+  get 'pages/all' => 'pages#all', :as => :all_pages
+  get 'sitemap' => 'pages#sitemap', :as => :sitemap_top
+  get 'pages/sitemap' => 'pages#sitemap', :as => :sitemap
+  match 'pages/:page_title' => 'pages#show', :as => :page
+  match 'feed.rss' => 'pages#feed', :as => :feed
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
